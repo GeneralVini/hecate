2192413a2d40bba895c6fe56cc3528bb772ca93b
GeneralVini <generalvini@gmail.com>
2026-09-10 21:25:22 -0300

    style: adequa User ao PSR-12

--- models/User.php
+++ models/User.php
@@ -1,14 +1,33 @@
 <?php
 
-namespace app\\models;
+namespace app\models;
 
-use yii\\web\\IdentityInterface;
+use yii\web\IdentityInterface;
 
 class User implements IdentityInterface
 {
-    public static function findIdentity($id) { return null; }
-    public static function findIdentityByAccessToken($token, $type = null) { return null; }
-    public function getId() { return null; }
-    public function getAuthKey() { return null; }
-    public function validateAuthKey($authKey) { return false; }
+    public static function findIdentity($id)
+    {
+        return null;
+    }
+
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        return null;
+    }
+
+    public function getId()
+    {
+        return null;
+    }
+
+    public function getAuthKey()
+    {
+        return null;
+    }
+
+    public function validateAuthKey($authKey)
+    {
+        return false;
+    }
 }
