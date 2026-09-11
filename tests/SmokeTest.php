473ba19f3b98d27138a7897434809019be4ba51c
GeneralVini <generalvini@gmail.com>
2026-09-10 23:34:32 -0300

    test: validate Yii3 ActiveRecord mappings as instance methods

--- tests/SmokeTest.php
+++ tests/SmokeTest.php
@@ -9,6 +9,7 @@ use App\Model\Division;
 use App\Model\Printer;
 use App\Model\Quota;
 use PHPUnit\Framework\TestCase;
+use ReflectionClass;
 
 final class SmokeTest extends TestCase
 {
@@ -19,9 +20,19 @@ final class SmokeTest extends TestCase
 
     public function testCoreActiveRecordMappings(): void
     {
-        self::assertSame('{{%contract}}', Contract::tableName());
-        self::assertSame('{{%division}}', Division::tableName());
-        self::assertSame('{{%printer}}', Printer::tableName());
-        self::assertSame('{{%quota}}', Quota::tableName());
+        self::assertSame('{{%contract}}', $this->tableNameWithoutConstructor(Contract::class));
+        self::assertSame('{{%division}}', $this->tableNameWithoutConstructor(Division::class));
+        self::assertSame('{{%printer}}', $this->tableNameWithoutConstructor(Printer::class));
+        self::assertSame('{{%quota}}', $this->tableNameWithoutConstructor(Quota::class));
+    }
+
+    /**
+     * @param class-string<Contract|Division|Printer|Quota> $className
+     */
+    private function tableNameWithoutConstructor(string $className): string
+    {
+        $model = (new ReflectionClass($className))->newInstanceWithoutConstructor();
+
+        return $model->tableName();
     }
 }
