8d14167ab5726dd88667661d890c6d22a865a1ab
GeneralVini <generalvini@gmail.com>
2026-09-10 21:48:29 -0300

    docs: declara atributos ActiveRecord de Printer

--- models/Printer.php
+++ models/Printer.php
@@ -1,17 +1,35 @@
 <?php
 
+declare(strict_types=1);
+
 namespace app\models;
 
 use yii\db\ActiveRecord;
 
+/**
+ * Modelo da impressora gerenciada pelo HECATE.
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $host
+ * @property string|null $vendor
+ * @property string|null $model
+ * @property string|null $location
+ * @property bool $is_color
+ * @property bool $is_duplex
+ * @property string|null $monitor_source
+ * @property string|null $last_seen_at
+ * @property bool $enabled
+ * @property string|null $created_at
+ */
 class Printer extends ActiveRecord
 {
-    public static function tableName()
+    public static function tableName(): string
     {
         return '{{%printer}}';
     }
 
-    public function rules()
+    public function rules(): array
     {
         return [
             [['name', 'host'], 'required'],
