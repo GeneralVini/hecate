5d6cbfca362e8012e845b81de653f2c3fdc91444
GeneralVini <generalvini@gmail.com>
2026-09-10 23:10:51 -0300

    refactor: declare Division tableName override

--- src/Model/Division.php
+++ src/Model/Division.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Model;
 
+use Override;
 use Yiisoft\ActiveRecord\ActiveRecord;
 
 final class Division extends ActiveRecord
@@ -13,6 +14,7 @@ final class Division extends ActiveRecord
     public string $name = '';
     public ?string $created_at = null;
 
+    #[Override]
     public function tableName(): string
     {
         return '{{%division}}';
