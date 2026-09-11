a2a834d3c31f06a373fe43459ed4671e051ebeef
GeneralVini <generalvini@gmail.com>
2026-09-10 22:48:57 -0300

    refactor: port Printer model to Yii3 ActiveRecord

---
+++ src/Model/Printer.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model;
+
+use Yiisoft\ActiveRecord\ActiveRecord;
+
+final class Printer extends ActiveRecord
+{
+    public ?int $id = null;
+    public string $name = '';
+    public string $host = '';
+    public ?string $vendor = null;
+    public ?string $model = null;
+    public ?string $location = null;
+    public bool $is_color = false;
+    public bool $is_duplex = false;
+    public ?string $monitor_source = null;
+    public ?string $last_seen_at = null;
+    public bool $enabled = true;
+    public ?string $created_at = null;
+
+    public function tableName(): string
+    {
+        return '{{%printer}}';
+    }
+}
