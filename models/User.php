51758e807c605910d290538be7bb82184a5948b3
GeneralVini <generalvini@gmail.com>
2026-09-10 21:46:33 -0300

    fix: adiciona tipos compatíveis ao modelo User

--- models/User.php
+++ models/User.php
@@ -1,32 +1,34 @@
 <?php
 
+declare(strict_types=1);
+
 namespace app\models;
 
 use yii\web\IdentityInterface;
 
 class User implements IdentityInterface
 {
-    public static function findIdentity($id)
+    public static function findIdentity($id): ?IdentityInterface
     {
         return null;
     }
 
-    public static function findIdentityByAccessToken($token, $type = null)
+    public static function findIdentityByAccessToken($token, $type = null): ?IdentityInterface
     {
         return null;
     }
 
-    public function getId()
+    public function getId(): int|string
     {
-        return null;
+        return '';
     }
 
-    public function getAuthKey()
+    public function getAuthKey(): string
     {
-        return null;
+        return '';
     }
 
-    public function validateAuthKey($authKey)
+    public function validateAuthKey($authKey): bool
     {
         return false;
     }
