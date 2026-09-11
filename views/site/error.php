c6dfdd74cc0c267be8674d59af744aa03f5317a9
GeneralVini <generalvini@gmail.com>
2026-09-10 22:54:56 -0300

    refactor: remove Yii2 error view

--- views/site/error.php
+++
@@ -1,5 +0,0 @@
-<?php
-use yii\\helpers\\Html;
-$this->title = 'Erro';
-?>
-<div class="panel-card"><h1>Erro</h1><p>O HECATE encontrou uma falha ao processar a solicitação.</p><p><?= Html::a('Voltar ao dashboard', ['/site/index'], ['class' => 'btn btn-primary']) ?></p></div>
