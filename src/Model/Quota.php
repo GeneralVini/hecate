44c0be180223116eb804e0b174d053c406224a9a
GeneralVini <generalvini@gmail.com>
2026-09-10 22:49:12 -0300

    refactor: port Quota model to Yii3 ActiveRecord

---
+++ src/Model/Quota.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model;
+
+use Yiisoft\ActiveRecord\ActiveRecord;
+
+final class Quota extends ActiveRecord
+{
+    public ?int $id = null;
+    public int $division_id = 0;
+    public string $period = '';
+    public int $bw_allocated = 0;
+    public int $bw_used = 0;
+    public int $bw_reserved = 0;
+    public int $color_allocated = 0;
+    public int $color_used = 0;
+    public int $color_reserved = 0;
+
+    public function tableName(): string
+    {
+        return '{{%quota}}';
+    }
+}
