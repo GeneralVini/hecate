5e216a50d904f80a16e969c1482d6fd4538c6974
GeneralVini <generalvini@gmail.com>
2026-09-10 21:25:14 -0300

    style: adequa Quota ao PSR-12

--- models/Quota.php
+++ models/Quota.php
@@ -1,17 +1,32 @@
 <?php
 
-namespace app\\models;
+namespace app\models;
 
-use yii\\db\\ActiveRecord;
+use yii\db\ActiveRecord;
 
 class Quota extends ActiveRecord
 {
-    public static function tableName() { return '{{%quota}}'; }
+    public static function tableName()
+    {
+        return '{{%quota}}';
+    }
+
     public function rules()
     {
         return [
             [['division_id', 'period'], 'required'],
-            [['division_id', 'bw_allocated', 'bw_used', 'bw_reserved', 'color_allocated', 'color_used', 'color_reserved'], 'integer'],
+            [
+                [
+                    'division_id',
+                    'bw_allocated',
+                    'bw_used',
+                    'bw_reserved',
+                    'color_allocated',
+                    'color_used',
+                    'color_reserved',
+                ],
+                'integer',
+            ],
             [['period'], 'string', 'max' => 7],
         ];
     }
