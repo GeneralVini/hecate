17807b1b3fce1ba636401e3ca62fd70237d3b88a
GeneralVini <generalvini@gmail.com>
2026-09-10 22:49:20 -0300

    refactor: port Contract model to Yii3 ActiveRecord

---
+++ src/Model/Contract.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model;
+
+use Yiisoft\ActiveRecord\ActiveRecord;
+
+final class Contract extends ActiveRecord
+{
+    public ?int $id = null;
+    public string $name = '';
+    public string $type = '';
+    public ?int $bw_quota = null;
+    public ?int $color_quota = null;
+    public ?string $bw_unit_price = null;
+    public ?string $color_unit_price = null;
+    public ?string $bw_overage_price = null;
+    public ?string $color_overage_price = null;
+    public bool $active = true;
+
+    public function tableName(): string
+    {
+        return '{{%contract}}';
+    }
+}
