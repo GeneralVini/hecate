c4d2c89e67441c428ef6071a080d666f786ca8c7
GeneralVini <generalvini@gmail.com>
2026-09-12 03:00:44 -0300

    build(assets): register HECATE discovery styles

--- src/Web/Shared/Layout/Main/MainAsset.php
+++ src/Web/Shared/Layout/Main/MainAsset.php
@@ -13,7 +13,7 @@ final class MainAsset extends AssetBundle
     public ?string $sourcePath = '@assetsSource/main';
 
     /** @var list<string> */
-    public array $css = ['site.css', 'login.css'];
+    public array $css = ['site.css', 'login.css', 'hecate-discovery.css'];
 
     /** @var list<string> */
     public array $js = ['site.js'];
