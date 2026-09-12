4b7bb51b33e54b580f890244676bbd298f3956da
GeneralVini <generalvini@gmail.com>
2026-09-12 17:05:09 -0300

    fix: corrige namespace da response factory PSR-17

--- src/Web/Demo/SelectScenario/Action.php
+++ src/Web/Demo/SelectScenario/Action.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Web\Demo\SelectScenario;
 
-use Psr\Http\Factory\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Yiisoft\Router\UrlGeneratorInterface;
