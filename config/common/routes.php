8a8b50fa42139db3445b2276c376c1219bcbadf1
GeneralVini <generalvini@gmail.com>
2026-09-11 23:52:18 -0300

    chore: adopt lefthook rector ecs qa baseline

--- config/common/routes.php
+++ config/common/routes.php
@@ -3,13 +3,14 @@
 declare(strict_types=1);
 
 use App\Web;
+use App\Web\Login\Action;
 use Yiisoft\Router\Group;
 use Yiisoft\Router\Route;
 
 return [
     Group::create()->routes(
         Route::get('/')
-            ->action(Web\Login\Action::class)
+            ->action(Action::class)
             ->name('login'),
         Route::get('/dashboard')
             ->action(Web\HomePage\Action::class)
--- src/bootstrap.php
+++ src/bootstrap.php
@@ -3,11 +3,12 @@
 declare(strict_types=1);
 
 use App\Environment;
+use Dotenv\Dotenv;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-if (empty($_ENV['APP_ENV']) && class_exists(\Dotenv\Dotenv::class)) {
-    \Dotenv\Dotenv::createImmutable(dirname(__DIR__))->safeLoad();
+if (empty($_ENV['APP_ENV']) && class_exists(Dotenv::class)) {
+    Dotenv::createImmutable(dirname(__DIR__))->safeLoad();
 }
 
 Environment::prepare();
