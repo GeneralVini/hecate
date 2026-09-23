e9f14ae49b1b19275d4a69b3fff88c1abd157476
GeneralVini <generalvini@gmail.com>
2026-09-23 18:13:38 -0300

    feat(assets): make main bundle depend on branding

--- src/Web/Shared/Layout/Main/MainAsset.php
+++ src/Web/Shared/Layout/Main/MainAsset.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Web\Shared\Layout\Main;
 
+use App\Web\Shared\Layout\Branding\BrandingAsset;
 use Yiisoft\Assets\AssetBundle;
 
 final class MainAsset extends AssetBundle
@@ -11,6 +12,9 @@ final class MainAsset extends AssetBundle
     public ?string $basePath = '@assets/main';
     public ?string $baseUrl = '@assetsUrl/main';
     public ?string $sourcePath = '@assetsSource/main';
+
+    /** @var list<class-string<AssetBundle>> */
+    public array $depends = [BrandingAsset::class];
 
     /** @var list<string> */
     public array $css = [
